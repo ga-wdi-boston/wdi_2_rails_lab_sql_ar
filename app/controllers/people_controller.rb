bae97ae016ad65bc082b91176346ff91c4e8aea0
David Fisher <tibbon@gmail.com>
2015-01-21 15:43:30 -0500

    Tests ready

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,17 +1,40 @@
 class PeopleController < ApplicationController
   def index
-    @people = People.all
+    @people = Person.all
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 
   def create
+    # people_params is a hash of first_name, last_name and age
+    @person = Person.create(people_params)
+
+    redirect_to @person # Do not alter this line
   end
 
   def update
+    # people_params is a hash of first_name, last_name and age
+    # First you'll want to find the person, then update their attributes
+    @person = Person.find(params[:id])
+    @person.update_attributes(people_params)
+
+    redirect_to @person # Do not alter this line
   end
 
   def destroy
+    # First you'll want to find the person
+    # Then delete them
+    Person.find(params[:id]).delete
+
+    redirect_to Person # Do not alter this line
+  end
+
+  private
+
+  # Do not alter this method
+  def people_params
+    params.require(:person).permit(:first_name, :last_name, :age)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    # Put your string of SQL in the blank string argument here
+    @products = Product.find_by_sql 'SELECT * FROM products'
   end
 
   def show
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+    # Put your string of SQL in the blank string argument here
+    # You may want to use string interpolation
+    # Remember that you can get the id of the incoming product with params[:id]
+    @product = Product.find_by_sql("SELECT * FROM products WHERE id = #{params[:id]}").first
   end
 end
