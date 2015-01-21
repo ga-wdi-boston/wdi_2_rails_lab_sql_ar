83ac793d7d86a7a9ff439c303977ae6860e4a281
David Fisher <tibbon@gmail.com>
2015-01-21 15:04:17 -0500

    Model specs really done

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+  subject(:person) { Person.create(first_name: 'David', last_name: 'Fisher', age: 32)}
   describe '#create' do
     it 'creates a new person, saving them to the DB' do
       person = Person.create
@@ -14,6 +15,20 @@ RSpec.describe Person, type: :model do
       person = Person.new
       expect(person).to be_a Person
       expect(person.persisted?).to be false
+    end
+  end
+
+  describe 'attributes from schema' do
+    it 'responds to first_name' do
+      expect(person.first_name).to be_a String
+    end
+
+    it 'responds to last_name' do
+      expect(person.last_name).to be_a String
+    end
+
+    it 'responds to age' do
+      expect(person.age).to be_a Fixnum
     end
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  subject(:product) { Product.create(name: "iPhone", price: 299.99) }
   describe '#create' do
     it 'creates a new product, saving them to the DB' do
       product = Product.create
@@ -14,6 +15,16 @@ RSpec.describe Product, type: :model do
       product = Product.new
       expect(product).to be_a Product
       expect(product.persisted?).to be false
+    end
+  end
+
+  describe 'attributes from schema' do
+    it 'responds to name' do
+      expect(product.name).to be_a String
+    end
+
+    it 'responds to price' do
+      expect(product.price).to be_a BigDecimal
     end
   end
 end
