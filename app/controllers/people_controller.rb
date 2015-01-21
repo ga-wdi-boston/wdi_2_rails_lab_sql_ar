580587a3f0ac8000b9e5a60cb9850bc4b468457f
David Fisher <tibbon@gmail.com>
2015-01-21 15:47:20 -0500

    Altered controllers

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,32 +1,35 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all
+    # Assign all instances of people to @people
   end
 
   def show
-    @person = Person.find(params[:id])
+    # Assign a person with a specific id from the parameters to @person
+    # Remember the id can come from params[:id]
   end
 
   def create
+    # Create a new person based on the incoming POST data
+    # Assign this new person to @person
+    # which you can access with people_params
     # people_params is a hash of first_name, last_name and age
-    @person = Person.create(people_params)
 
     redirect_to @person # Do not alter this line
   end
 
   def update
+    # Update the attributes of a single person from the PUT action
     # people_params is a hash of first_name, last_name and age
-    # First you'll want to find the person, then update their attributes
-    @person = Person.find(params[:id])
-    @person.update_attributes(people_params)
+    # First you'll want to find the person, assign them to @person
+    # then update their attributes
 
     redirect_to @person # Do not alter this line
   end
 
   def destroy
+    # Delete a person record from the DELETE action
     # First you'll want to find the person
     # Then delete them
-    Person.find(params[:id]).delete
 
     redirect_to Person # Do not alter this line
   end
