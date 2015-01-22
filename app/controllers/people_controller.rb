1b98b3e642c0fc1a4fef51aa602e349d5f6a7035
Max Blaushild <blaushmild@gmail.com>
2015-01-22 15:47:13 -0500

    finishes the assingment

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,13 @@
 class PeopleController < ApplicationController
   def index
     # Assign all instances of people to @people
+    @people = Person.all
   end
 
   def show
     # Assign a person with a specific id from the parameters to @person
     # Remember the id can come from params[:id]
+    @person = Person.find(params[:id])
   end
 
   def create
@@ -13,6 +15,7 @@ class PeopleController < ApplicationController
     # Assign this new person to @person
     # which you can access with people_params
     # people_params is a hash of first_name, last_name and age
+    @person = Person.create(people_params)
 
     redirect_to @person # Do not alter this line
   end
@@ -22,6 +25,8 @@ class PeopleController < ApplicationController
     # people_params is a hash of first_name, last_name and age
     # First you'll want to find the person, assign them to @person
     # then update their attributes
+    @person = Person.find(params[:id])
+    @person.update(people_params)
 
     redirect_to @person # Do not alter this line
   end
@@ -30,6 +35,8 @@ class PeopleController < ApplicationController
     # Delete a person record from the DELETE action
     # First you'll want to find the person
     # Then delete them
+    @person = Person.find(params[:id])
+    @person.delete
 
     redirect_to Person # Do not alter this line
   end
