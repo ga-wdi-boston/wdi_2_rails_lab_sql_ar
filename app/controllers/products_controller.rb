6ab7da1c934c2598b0b629d34674db7d1436c0a5
J.Marx <jennyrutledge@gmail.com>
2015-01-22 15:52:02 -0500

    Complete people rails lab.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,13 @@
 class ProductsController < ApplicationController
   def index
     # Put your string of SQL in the blank string argument here
-    @products = Product.find_by_sql ''
+    @products = Product.find_by_sql 'SELECT * FROM products'
   end
 
   def show
     # Put your string of SQL in the blank string argument here
     # You may want to use string interpolation
     # Remember that you can get the id of the incoming product with params[:id]
-    @product = Product.find_by_sql("").first
+    @product = Product.find_by_sql("SELECT * FROM products where id = #{params[:id]}").first
   end
 end
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'byebug'
 
 RSpec.describe PeopleController do
 
